03284a06e68649da4c2d638bd84351493e848cbd
Jaguar J <jaguarjung95@gmail.com>
2019-09-21 19:14:37 -0700

    Updated app.js  and content with new components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { Header } from "./components/layout/Header";
+import { Content } from "./components/layout/Content";
+import { ProjectsProvider, SelectedProjectProvider } from "./context";
 
 export const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <p>Hello!</p>
-    </header>
-  </div>
+  <SelectedProjectProvider>
+    <ProjectsProvider>
+      <div className="App">
+        <Header></Header>
+        <Content></Content>
+      </div>
+    </ProjectsProvider>
+  </SelectedProjectProvider>
 );
 
 export default App;

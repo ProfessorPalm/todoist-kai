f8a3185a3a5a6c1c7ad0a759bfbab01b2a6c0118
Jaguar J <jaguarjung95@gmail.com>
2019-09-21 17:53:47 -0700

    firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCh2GmYHEybGkp8_BQqyrb5g8n5tWBKmwg",
+  authDomain: "todoist-kai.firebaseapp.com",
+  databaseURL: "https://todoist-kai.firebaseio.com",
+  projectId: "todoist-kai",
+  storageBucket: "todoist-kai.appspot.com",
+  messagingSenderId: "358959332067",
+  appId: "1:358959332067:web:0726ae6a6b9440370123d6"
+};
+
+export { firebaseConfig as firebase };

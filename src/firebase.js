e7977389737d0c57defda9fb2e6810a0c61786b7
Jaguar J <jaguarjung95@gmail.com>
2019-09-21 19:05:45 -0700

    fixed firebase initializer

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
 
-const firebaseConfig = {
+const firebaseConfig = firebase.initializeApp({
   apiKey: "AIzaSyCh2GmYHEybGkp8_BQqyrb5g8n5tWBKmwg",
   authDomain: "todoist-kai.firebaseapp.com",
   databaseURL: "https://todoist-kai.firebaseio.com",
@@ -9,6 +9,6 @@ const firebaseConfig = {
   storageBucket: "todoist-kai.appspot.com",
   messagingSenderId: "358959332067",
   appId: "1:358959332067:web:0726ae6a6b9440370123d6"
-};
+});
 
 export { firebaseConfig as firebase };
